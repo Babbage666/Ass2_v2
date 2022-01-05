58c1999ab082061cb13fa51c3b7fb0fcd0ae43e7
Babbage666 <94875874+Babbage666@users.noreply.github.com>
2022-01-06 09:50:24 +1100

    Tidy up.

--- Program.cs
+++ Program.cs
@@ -13,8 +13,8 @@ namespace KIT206_RAP_Project
 {
     /* TODO 
        1. Publications list in researcher details needs to be able to be filtered by year and sorted. NOT DONE.
-       4. General Error catching (ie, unexpected input etc)
-       5. WPF implementation needs to be included and completed
+       4. General Error catching (ie, unexpected input etc) NOT DONE
+       5. WPF implementation needs to be included and completed NOT DONE
      * 
      */
   
